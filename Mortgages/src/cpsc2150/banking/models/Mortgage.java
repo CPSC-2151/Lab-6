71b0534a0be4bcce17941a9b511df85ab0d00dcd
DesC0DE <jaden.humphries@gmail.com>
2023-10-24 11:57:39 -0400

    Set up structure for Mortgages.java for IMortgage.java implementation

--- Mortgages/src/cpsc2150/banking/models/Mortgage.java
+++ Mortgages/src/cpsc2150/banking/models/Mortgage.java
@@ -1,7 +1,29 @@
 package cpsc2150.banking.models;
 
-public class Mortgage extends AbsMortgage implements IMortgage {
+public class Mortgage extends AbsMortgage implements IMortgage{
 
+    @Override
+    public boolean loanApproved() {
+        return false;
+    }
 
+    @Override
+    public double getPayment() {
+        return 0;
+    }
 
+    @Override
+    public double getRate() {
+        return 0;
+    }
+
+    @Override
+    public double getPrincipal() {
+        return 0;
+    }
+
+    @Override
+    public int getYears() {
+        return 0;
+    }
 }
